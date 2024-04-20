3d0e3e59ac2c7227d77faddd64b1549727d716b5
hussainraza22 <103336904+hussainraza22@users.noreply.github.com>
2024-04-20 17:28:05 +0500

    first commit

---
+++ well-known/search.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="search/search.css">
+    <link rel="stylesheet" href="css/style.css">
+
+        <script src="js/jquery.js"></script>
+        <script src="js/jquery-migrate-1.1.1.js"></script>
+        <script src="js/superfish.js"></script>
+        <script src="js/jquery.mobilemenu.js"></script>
+
+        <script src="js/forms.js"></script>
+        <script src="js/jquery.easing.1.3.js"></script>
+        <script src="js/jquery.ui.totop.js"></script>
+        <script src="search/search.js"></script>
+	<!--[if lt IE 8]>
+        <div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/img/upgrade.jpg"border="0"alt=""/></a></div>  
+    <![endif]-->
+  <!--[if lt IE 9]>
+    <link rel="stylesheet" href="css/ie.css">
+    <script src="assets/js/html5shiv.js"></script>
+  <![endif]-->
+</head>
+<body>
+<!-- header -->
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="span10 offset1">                
+                <h1 class="brand margin16"><a href="index.html"><img src="img/logo.png" alt="Nicolas Smith professional photographer"></a></h1>
+<!--menu-->
+                <div class="navbar">
+                     <div class="navbar-inner"> 
+                        <div class="nav-collapse nav-collapse_ collapse">
+                            <ul class="sf-menu margin3">
+                                <li><a href="index.html"><span>Home</span></a></li>
+                                <li class="sub-menu"><a href="index-1.html"><span>About</span></a>
+                                    <ul class="nav sub-menu">
+                                        <li><a href="#">awards</a></li>
+                                        <li class="sub-menu"><a href="#">my News</a>
+                                            <ul class="nav sub-menu">
+                                                <li><a href="#">Latest</a></li>
+                                                <li class="last"><a href="#">archive</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Testimonials</a></li>
+                                        <li><a href="#">clients</a></li>
+                                        <li class="last"><a href="#">Vision</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="index-2.html"><span>resume</span></a></li>
+                                <li><a href="index-3.html"><span>Works</span></a></li>
+                                <li><a href="index-4.html"><span>Blog</span></a></li>
+                                <li class="last"><a href="index-5.html"><span>Contacts</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- content -->
+<div class="bgPic"><img src="img/page1_slide1.jpg" alt></div>
+<div id="content">  
+    <div class="bg2">  
+        <div class="container">
+            <div class="row">
+                <div class="span12">
+                	<h2>Search results:</h2>
+					<div id="search-results"></div>
+                </div>
+            </div>
+        </div>     
+    </div>
+</div>
+<!-- footer -->
+<footer>
+    <div>  
+        <div class="bg3">
+            <div class="container">
+                <div class="row">
+                    <div class="span12">
+                        <p class="pull-right padding1 phone">Phone: +1-800-7575-432</p>
+                        <p class="color1">&copy; 2013 | <a href="index-6.html">Privacy policy</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
